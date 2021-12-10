4bb9385da7eda4da6e83fc25d5e9e803b6cd566e
Yusuke Sakai <sakai.yusuke@classmethod.jp>
2021-12-11 06:10:51 +0900

    feat: リソース定義を追加

--- lib/cdk-snapshot-test-sample-stack.ts
+++ lib/cdk-snapshot-test-sample-stack.ts
@@ -1,13 +1,35 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { BillingMode } from "aws-cdk-lib/aws-dynamodb";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class CdkSnapshotTestSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // DynamoDB
+    const sampleTable = new dynamodb.Table(this, "sampleTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // Lambda
     const sampleLambda = new NodejsFunction(this, "sampleLambda", {
       entry: "lib/sample-lambda.handler.ts",
+      environment: {
+        TABLE_NAME: sampleTable.tableName,
+      },
     });
+    sampleTable.grantReadData(sampleLambda);
+
+    // API Gateway
+    const sampleApi = new apigateway.RestApi(this, "sample-api");
+    sampleApi.root.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(sampleLambda)
+    );
   }
 }
--- lib/sample-lambda.handler.ts
+++ lib/sample-lambda.handler.ts
@@ -1,3 +1,18 @@
-export const handler = async (): Promise<void> => {
-  console.log("sample lambda handler!");
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const client = new DocumentClient({ region: "ap-northeast-1" });
+  const res = await client
+    .scan({
+      TableName: process.env.TABLE_NAME as string,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(res.Items ?? []),
+  };
 };
